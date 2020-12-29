87fab728a14980cb0902966b2adddaaca8f92caa
黑键 <jj89757hhb.happy@163.com>
2020-12-29 15:15:16 +0800

    0.1.2

---
+++ Example/ZDFoundation/ZDTestCell.h
@@ -0,0 +1,17 @@
+//
+//  ZDTestCell.h
+//  ZDFoundation_Example
+//
+//  Created by hhb201708 on 2020/12/29.
+//  Copyright © 2020 jj89757hhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZDTestCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/ZDFoundation/ZDTestCell.m
@@ -0,0 +1,24 @@
+//
+//  ZDTestCell.m
+//  ZDFoundation_Example
+//
+//  Created by hhb201708 on 2020/12/29.
+//  Copyright © 2020 jj89757hhb. All rights reserved.
+//
+
+#import "ZDTestCell.h"
+
+@implementation ZDTestCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
